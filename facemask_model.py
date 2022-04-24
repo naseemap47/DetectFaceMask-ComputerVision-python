516c5c962ea19b1d7161007455785baebaa8d203
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-24 18:08:24 +0530

    impoted create_generator function from my_utils to Create Train, val and Test generators

--- facemask_model.py
+++ facemask_model.py
@@ -1,7 +1,15 @@
 from my_utils import sample_images
+from my_utils import create_generators
 
-Sample = True
+Sample = False
 
 if Sample:
     path_img = '/home/naseem/PycharmProjects/DetectFaceMask-ComputerVision-python/FaceMaskDataset/Test/WithMask'
     sample_images(path_img)
+
+train_generators, val_generators, test_generators = create_generators(
+    batch_size=32,
+    path_to_train_data='/home/naseem/PycharmProjects/DetectFaceMask-ComputerVision-python/FaceMaskDataset/Train',
+    path_to_val_data='/home/naseem/PycharmProjects/DetectFaceMask-ComputerVision-python/FaceMaskDataset/Validation',
+    path_to_test_data='/home/naseem/PycharmProjects/DetectFaceMask-ComputerVision-python/FaceMaskDataset/Test'
+)
