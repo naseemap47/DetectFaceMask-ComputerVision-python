e725fff24d508d841983f382e0e74e5c097b8078
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-24 17:49:24 +0530

    imported sample_images function from my_utils, to show sample images from data set

--- facemask_model.py
+++ facemask_model.py
@@ -1,1 +1,5 @@
-print('Done')
+from my_utils import sample_images
+
+
+path_img = '/home/naseem/PycharmProjects/DetectFaceMask-ComputerVision-python/FaceMaskDataset/Test/WithMask'
+sample_images(path_img)
