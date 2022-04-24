c5d6132e54700a3b7a95efbded9fb4be738baae6
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-24 17:50:59 +0530

    added switch for show sample dataset

--- facemask_model.py
+++ facemask_model.py
@@ -1,5 +1,7 @@
 from my_utils import sample_images
 
+Sample = True
 
-path_img = '/home/naseem/PycharmProjects/DetectFaceMask-ComputerVision-python/FaceMaskDataset/Test/WithMask'
-sample_images(path_img)
+if Sample:
+    path_img = '/home/naseem/PycharmProjects/DetectFaceMask-ComputerVision-python/FaceMaskDataset/Test/WithMask'
+    sample_images(path_img)
