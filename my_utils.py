e0bfb72ea88d76d7b3a3dce2d2e0cb4a9fddb718
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-24 18:05:48 +0530

    Writen function to Create Train, val and Test generators

--- my_utils.py
+++ my_utils.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from keras.preprocessing.image import load_img
 import os
+from keras.preprocessing.image import ImageDataGenerator
 
 
 def sample_images(img_folder_path):
@@ -15,3 +16,39 @@ def sample_images(img_folder_path):
         plt.tight_layout()
         plt.axis('off')
     plt.show()
+
+
+def create_generators(batch_size, path_to_train_data, path_to_val_data, path_to_test_data):
+    train_preprocessor = ImageDataGenerator(
+        rescale=1/255,
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1
+    )
+    val_preprocessor = ImageDataGenerator(rescale=1/255)
+    test_preprocessor = ImageDataGenerator(rescale=1/255)
+    train_generators = train_preprocessor.flow_from_directory(
+        path_to_train_data,
+        target_size=(70, 70),
+        color_mode='rgb',
+        class_mode='categorical',
+        batch_size=batch_size,
+        shuffle=True
+    )
+    val_generators = val_preprocessor.flow_from_directory(
+        path_to_val_data,
+        target_size=(70, 70),
+        color_mode='rgb',
+        class_mode='categorical',
+        batch_size=batch_size,
+        shuffle=False
+    )
+    test_generators = test_preprocessor.flow_from_directory(
+        path_to_test_data,
+        target_size=(70, 70),
+        color_mode='rgb',
+        class_mode='categorical',
+        batch_size=batch_size,
+        shuffle=False
+    )
+    return train_generators, val_generators, test_generators
