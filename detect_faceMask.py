29a5b67d3a7f9eb438bfc68fe19ff6dd9e90c2ed
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-25 14:51:24 +0530

    get x, y, w, h of faces

--- detect_faceMask.py
+++ detect_faceMask.py
@@ -1,10 +1,16 @@
 import cv2
 
+face_classifier = cv2.CascadeClassifier(
+    '/home/naseem/PycharmProjects/DetectFaceMask-ComputerVision-python/haarcascade_frontalface_default.xml'
+)
 cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    faces = face_classifier.detectMultiScale(img_rgb)
+    print(faces)
+    
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
